92d1df4eff6f6c21de4c44ba5097495c01aa74c7
Alexandr G <alexandr@gavrisco.com>
2017-01-19 17:58:46 +0200

    Add script to get ascii models.

---
+++ models/get-ascii-models.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Download Torch models in ascii format and save them in binary format.
+
+cd "$(dirname "$0")"
+
+mkdir -p openface && cd openface
+
+printf "\n\n====================================================\n"
+printf "Downloading OpenFace models, which are copyright\n"
+printf "Carnegie Mellon University and are licensed under\n"
+printf "the Apache 2.0 License.\n\n"
+printf "This will incur about 100MB of network traffic for the models.\n"
+printf "====================================================\n\n"
+
+wget -nv http://openface-models.storage.cmusatyalab.org/nn4.small2.v1.ascii.t7.xz \
+  -O nn4.small2.v1.ascii.t7.xz
+[ $? -eq 0 ] || ( rm nn4.small2.v1.t7.xz* && die "+ nn4.small2.v1.ascii.t7.xz: Error in wget." )
+unxz nn4.small2.v1.ascii.t7.xz && \
+./convert-ascii-model.lua nv4.small2.v1.ascii.t7 nv4.small nv4.small2.v1.t7 && \
+rm nv4.small2.v1.ascii.t7
